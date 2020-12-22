b588dd9cbbf0f373dad4971f4079c9f4bdfb379f
unparalleled-js <yingthi@live.com>
2020-12-22 17:51:11 -0600

    remove unused parts

--- Wilder/Program.cs
+++ Wilder/Program.cs
@@ -1,25 +1,10 @@
-﻿using System;
-using Wilder.Controllers;
-using Wilder.Projects;
-
-namespace Wilder
+﻿namespace Wilder
 {
-    class MainClass
+    internal static class MainClass
     {
         public static void Main(string[] args)
         {
             var core = new Core();
-            core.ProjectParser.Parse("")
-            // var projects = controller.GetProjects();
-            // var index = 1;
-            // Console.WriteLine($"You have {projects.Count} projects:");
-            // foreach (var project in projects)
-            // {
-            //     var projectString = $"{index}: '{project.Title}' by {project.Author}.";
-            //     Console.WriteLine(projectString);
-            //     Console.WriteLine(project.Genre);
-            //     index += 1;
-            // }
         }
     }
 }
