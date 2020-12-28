8de1c0624ac602477154ada2f2ecd3a517a617c6
unparalleled-js <yingthi@live.com>
2020-12-28 11:17:17 -0600

    WIP

--- wilderpy/src/wilder/server/__init__.py
+++ wilderpy/src/wilder/server/__init__.py
@@ -1,12 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask import request
 
 from wilder import get_mgmt_json
+from wilder import get_mgmt
+from wilder.server.error import WildServerError, MissingArtistError, MissingAlbumError
 from wilder.server.logger import get_error_file_logger
 
 
 app = Flask(__name__)
 
 
+@app.errorhandler(WildServerError)
+def handle_invalid_usage(err):
+    response = jsonify(err.dict())
+    response.status_code = err.status_code
+    return response
+
+
 @app.route('/')
 def mgmt():
     return get_mgmt_json(as_dict=False)
+
+
+@app.route('/sign', methods=['POST'])
+def sign():
+    if request.method == 'POST':
+        data = request.form
+        mgmt = get_mgmt()
+        mgmt.start_new_album()
+        
+
+def _verify_sign_request_data(data):
+    if not data.get("artist"):
+        raise MissingArtistError()
+    if not data.get("albums"):
+        raise MissingAlbumError()
---
+++ wilderpy/src/wilder/server/error.py
@@ -0,0 +1,27 @@
+class WildServerError(Exception):
+    def __init__(self, status_code, message, payload=None):
+        Exception.__init__(self)
+        self.status_code = status_code
+        self.message = message
+        self.payload = payload
+
+    @property
+    def dict(self):
+        rv = dict(self.payload or ())
+        rv["message"] = self.message
+        return rv
+    
+
+class WildBadRequestError(WildServerError):
+    def __init__(self, message, payload=None):
+        WildServerError.__init__(self, 400, message, payload)
+
+
+class MissingArtistError(WildBadRequestError):
+    def __init__(self):
+        WildServerError.__init__(self, 400, "MISSING_ARTIST")
+
+
+class MissingAlbumError(WildBadRequestError):
+    def __init__(self):
+        WildServerError.__init__(self, 400, "MISSING_ALBUM")
