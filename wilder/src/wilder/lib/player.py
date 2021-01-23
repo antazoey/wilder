0abfae6a8f339852d9abe963bf79cd6fa089f3db
unparalleled-js <yingthi@live.com>
2021-01-22 22:03:03 -0600

    wip

--- wilder/src/wilder/lib/player.py
+++ wilder/src/wilder/lib/player.py
@@ -1,4 +1,7 @@
+import time
+
 import vlc
+from wilder.lib.errors import WildError
 
 
 def play_album(album, audio_type):
@@ -8,5 +11,23 @@ def play_album(album, audio_type):
 
 
 def play_track(track_path):
-    player = vlc.MediaPlayer(track_path)
-    player.play()
+    player = vlc.MediaPlayer()
+    media = vlc.Media(track_path)
+    player.set_media(media)
+
+    cmd_res = player.play()
+    if cmd_res == -1:
+        raise WildError("Player failed to launch.")
+
+    start = 1
+    time.sleep(start)
+    track_length = player.get_length() * 0.001
+
+    remaining = track_length - start
+    while remaining > 0:
+        wait_time = 0.1
+        print(f"\r{round(remaining)}           \r", end="\r")
+        time.sleep(wait_time)
+        remaining -= wait_time
+
+    print()
