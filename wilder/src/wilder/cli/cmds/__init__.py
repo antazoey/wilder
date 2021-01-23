620adcac116cda17aa27b9623fb1ceabdb827f55
unparalleled-js <yingthi@live.com>
2021-01-23 13:20:08 -0600

    Set up for handler

--- wilder/src/wilder/cli/cmds/__init__.py
+++ wilder/src/wilder/cli/cmds/__init__.py
@@ -2,7 +2,7 @@ import click
 from wilder.lib.constants import Constants
 from wilder.lib.errors import ArtistNotFoundError
 from wilder.lib.errors import NoArtistsFoundError
-from wilder.lib.mgmt.album_dir import get_album_json
+from wilder.lib.mgmt.album_dir import get_album_directory
 
 
 class ArtistArgRequiredIfGivenCommand(click.Command):
@@ -18,7 +18,11 @@ class AlbumDirCommand(ArtistArgRequiredIfGivenCommand):
         wilder = ctx.obj.wilder  # This line must stay to load context
         album_arg = ctx.params.get(Constants.ALBUM)
         artist_arg = ctx.params.get(Constants.ARTIST)
-        album_json = get_album_json(wilder, artist_arg, album_arg)
+        album_dir = get_album_directory(wilder, get_default_handler=self._select_album_from_list)
+        album_json = album_dir.get_album_json(artist_arg, album_arg)
         ctx.params[Constants.ALBUM] = album_json[Constants.NAME]
         ctx.params[Constants.ARTIST] = album_json[Constants.ARTIST]
         return super().invoke(ctx)
+
+    def _select_album_from_list(self):
+        raise Exception("TADFASDFG")
--- wilder/src/wilder/lib/mgmt/album_dir.py
+++ wilder/src/wilder/lib/mgmt/album_dir.py
@@ -94,51 +94,88 @@ def echo_tracks(tracks):
         click.echo(f"{track.track_number}. {track.name}")
 
 
-def get_album_json(wilder, artist_arg, album_arg):
-    """Get the album directory JSON blob. If given an album name, it uses the Wilder SDK.
-    Otherwise, it attempts to figure out the artist/album combo based on the currently working
-    directory.
-
-    Cases:
-        * You provide the album arg: It uses the Wilder SDK to find the path to the album to get the rest of the data.
-        * You are in the album directory: It uses the local album.json file.
-        * You are in the album directory and the album.json does not exist: It recreates the default JSON and uses that.
-        * You are in a track directory of an album. It uses the parent album.json file if it exists.
-        * You are in a track directory of an album and the album.json does not exist in the parent directorY: It will
-            create the default album JSON in the parent directory and use that.
-    """
-
-    if not album_arg:
-        try:
-            album_json = _try_get_local_json()
-            if not album_json:
-                raise NotInAlbumError()
-            return album_json
-        except OSError:
-            # Handles case when in a strange directory
-            raise NotInAlbumError()
-    else:
-        album = wilder.get_album(album_arg, artist_name=artist_arg)
-        return load_json_from_file(album.dir_json_path)
+def get_album_directory(wilder, get_default_handler=None):
+    return AlbumDirectory(wilder, get_default_handler=get_default_handler)
 
 
-def _try_get_local_json():
-    here = os.getcwd()
-    album_json_path, album_json = _get_album_json_from_here(here)
-    if album_json:
-        return album_json
+class AlbumDirectory:
+    def __init__(self, wilder, get_default_handler=None):
+        self.wilder = wilder
+        self._get_default = get_default_handler
 
-    local_json = _create_album_if_should_exist(here, album_json_path)
-    if local_json:
-        return local_json
+    def get_album_json(self, artist_arg, album_arg):
+        """Get the album directory JSON blob. If given an album name, it uses the Wilder SDK.
+        Otherwise, it attempts to figure out the artist/album combo based on the currently working
+        directory.
 
-    return _try_get_local_json_from_parent_dir(here)
+        Cases:
+            * You provide the album arg: It uses the Wilder SDK to find the path to the album to get the rest of the data.
+            * You are in the album directory: It uses the local album.json file.
+            * You are in the album directory and the album.json does not exist: It recreates the default JSON and uses that.
+            * You are in a track directory of an album. It uses the parent album.json file if it exists.
+            * You are in a track directory of an album and the album.json does not exist in the parent directorY: It will
+                create the default album JSON in the parent directory and use that.
+            * You provide a handler for getting the default Album Directory. An example would be to allow the user to
+                select from a list.
+        """
+
+        if not album_arg:
+            try:
+                album_json = _try_get_local_json()
+                if not album_json:
+                    return self._handle_could_not_find()
+                return album_json
+            except OSError:
+                # Handles case when in a strange directory
+                return self._handle_could_not_find()
+        else:
+            album = self.wilder.get_album(album_arg, artist_name=artist_arg)
+            return load_json_from_file(album.dir_json_path)
+
+    def _handle_could_not_find(self):
+        if self._get_default:
+            return self._get_default()
+        return self._handle_could_not_find()
 
 
-def _get_album_json_from_here(here):
+def _get_album_json_from_here(here=None):
+    here = here or os.getcwd()
     album_json_path = get_album_json_path(here)
     album_json = _get_album_json_from_path(album_json_path)
     return album_json_path, album_json
+
+
+def _album_is_supposed_to_exist(here=None):
+    mgmt_json = get_mgmt_json()
+    artists = mgmt_json.get(Constants.ARTISTS)
+    here = here or os.getcwd()
+    for artist in artists:
+        albums = artist.get(Constants.DISCOGRAPHY)
+        for album in albums:
+            path = album.get(Constants.PATH)
+            name = album.get(Constants.NAME)
+            if path == here:
+                return name
+
+
+def _create_album_if_should_exist(album_json_path, here=None):
+    here = here or os.getcwd()
+    name = _album_is_supposed_to_exist(here=here)
+    if name:
+        init_album_dir(here, name)
+        return load_json_from_file(album_json_path)
+
+
+def _try_get_local_json():
+    album_json_path, album_json = _get_album_json_from_here()
+    if album_json:
+        return album_json
+
+    local_json = _create_album_if_should_exist(album_json_path)
+    if local_json:
+        return local_json
+
+    return _try_get_local_json_from_parent_dir()
 
 
 def _get_album_json_from_path(path):
@@ -146,30 +183,10 @@ def _get_album_json_from_path(path):
         return load_json_from_file(path)
 
 
-def _create_album_if_should_exist(here, album_json_path):
-    should_exist, name = _album_is_supposed_to_exist(here)
-    if should_exist and name:
-        init_album_dir(here, name)
-        return load_json_from_file(album_json_path)
-
-
-def _album_is_supposed_to_exist(here):
-    mgmt_json = get_mgmt_json()
-    artists = mgmt_json.get(Constants.ARTISTS)
-    for artist in artists:
-        albums = artist.get(Constants.DISCOGRAPHY)
-        for album in albums:
-            path = album.get(Constants.PATH)
-            name = album.get(Constants.NAME)
-            if path == here:
-                return True, name
-    return False, None
-
-
-def _try_get_local_json_from_parent_dir(here):
+def _try_get_local_json_from_parent_dir(here=None):
+    here = here or os.getcwd()
     here = get_parent(here)
-    album_json_path, album_json = _get_album_json_from_here(here)
+    album_json_path, album_json = _get_album_json_from_here(here=here)
     if album_json:
         return album_json
-    else:
-        return _create_album_if_should_exist(here, album_json_path)
+    return _create_album_if_should_exist(album_json_path)
